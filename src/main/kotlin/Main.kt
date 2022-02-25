d04c45ef6a9eb9fbd55824268fd7621c9fdedb6d
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-02-24 19:22:45 -0600

    User can give a password to hide the message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+package cryptography
+
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
@@ -30,10 +32,13 @@ fun printMenu() {
     println("Bye!")
 }
 
+/* Shows the message for a given image */
 fun showMessage() {
     println("Input image file:")
     val fileName = readLine()!!
 
+    println("Password:")
+    val password = readLine()!!
     val image: BufferedImage
     try {
         image = ImageIO.read(File(fileName))
@@ -41,24 +46,35 @@ fun showMessage() {
         println("Can't read input file!")
         return
     }
-    readMessageInImage(image = image)
+    readMessageInImage(image = image, password = password)
 }
 
 /* Look for the bits in the blue number of each pixel */
-fun readMessageInImage(image: BufferedImage) {
+fun readMessageInImage(image: BufferedImage, password: String) {
+    val passwordAsArrayOfBits = messageToBits(message = password)
     val bitsFromImageAsArray: String = bitsFromImage(image = image)
     // Make a substring until the first occurrence of ESCAPE_PATRON
     val messageAsBits = bitsFromImageAsArray.substring(0, bitsFromImageAsArray.indexOf(ESCAPE_PATRON))
-    val message = bitsToMessage(messageAsBits = messageAsBits)
+    val message = bitsToMessage(messageAsBits = messageAsBits, passwordAsArrayOfBits = passwordAsArrayOfBits)
     println("Message:")
     println(message)
 }
 
 /* It will traduce the array of bits to a message */
-fun bitsToMessage(messageAsBits: String): String {
+fun bitsToMessage(messageAsBits: String, passwordAsArrayOfBits: List<Int>): String {
     val message = StringBuilder()
-    for (i in messageAsBits.indices step 8) {
-        val byte = messageAsBits.substring(i, i + 8) // Creates a group of 8 bits
+    val messageEncrypted = messageAsBits.map { it.toString().toInt() } // Creates a int collection
+    val auxMessageList = mutableListOf<Int>()
+    var j = 0
+    for (i in messageEncrypted.indices) {
+        if (j == passwordAsArrayOfBits.size) j = 0
+        auxMessageList.add(messageEncrypted[i].xor(passwordAsArrayOfBits[j]))
+        j++
+    }
+    val auxMessage = auxMessageList.joinToString("")
+    // Translate bytes to message
+    for (i in auxMessage.indices step 8) {
+        val byte = auxMessage.substring(i, i + 8) // Creates a group of 8 bits
         message.append(byte.toInt(2).toChar()) // It cast a binary number to its char form
     }
     return message.toString()
@@ -95,8 +111,13 @@ fun hideMessage() {
 
     println("Message to hide:")
     val message = readLine()!!
+    println("Password:")
+    val password = readLine()!!
 
-    val hidden = hideMessageInImage(image = image, message = message)
+    // Creates a password that repeats itself the necessary times to match with message length
+    val passwordUpdated = password.repeat(message.length / password.length) + password.substring(0, message.length.rem(password.length))
+
+    val hidden = hideMessageInImage(image = image, message = message, password = passwordUpdated)
 
     if (hidden) {
         ImageIO.write(image, "png", File(newFileName))
@@ -108,8 +129,12 @@ fun hideMessage() {
 
 /* It goes into the pixel image and calls the setLeastSignificantBitToOne
 for each color in each pixel */
-fun hideMessageInImage(image: BufferedImage, message: String): Boolean{
-    val messageAsArrayOfBytes: List<Int> = messageToBits(message = message)
+fun hideMessageInImage(image: BufferedImage, message: String, password: String): Boolean{
+    val messageAsArrayOfBytes: MutableList<Int> = messageToBits(message = message).toMutableList()
+    val escapePatron = listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1)
+    messageAsArrayOfBytes.addAll(escapePatron) // adds the escape patron to message array
+    val passwordAsArrayOfBytes: List<Int> = messageToBits(message = password)
+
 
     if (messageAsArrayOfBytes.size < image.width * image.height) {
         var count = 0
@@ -117,12 +142,22 @@ fun hideMessageInImage(image: BufferedImage, message: String): Boolean{
             for (x in 0 until image.width) {
                 val color = Color(image.getRGB(x, y))
                 if (count < messageAsArrayOfBytes.size) {
+                    val newColor = if (count < passwordAsArrayOfBytes.size) { // while count is in password length
+                        Color(
+                            color.red,
+                            color.green,
+                            setLastBitWithXor(pixel = color.blue,
+                                bitMessage = messageAsArrayOfBytes[count],
+                                bitPassword = passwordAsArrayOfBytes[count])
+                        ).rgb
+                    } else { // Out of password length, for the escape patron
+                        Color(
+                            color.red,
+                            color.green,
+                            setLastBit(pixel = color.blue, bit = messageAsArrayOfBytes[count])
+                        ).rgb // Get the rgb value
+                    }
 
-                    val newColor = Color(
-                        color.red,
-                        color.green,
-                        setLastBit(pixel = color.blue, bit = messageAsArrayOfBytes[count])
-                    ).rgb // Get the rgb value
 
                     image.setRGB(x, y, newColor)
                     count++
@@ -138,6 +173,11 @@ fun hideMessageInImage(image: BufferedImage, message: String): Boolean{
     }
 }
 
+fun setLastBitWithXor(pixel: Int, bitMessage: Int, bitPassword: Int): Int {
+    val bitAfterXor = bitMessage.xor(bitPassword)
+    return setLastBit(pixel = pixel, bit = bitAfterXor)
+}
+
 /* Change the last bit of the color following the bit input */
 fun setLastBit(pixel: Int, bit: Int) = pixel.and(254).or(bit)
 
@@ -148,7 +188,7 @@ fun messageToBits(message: String): List<Int> {
     // It will change each int in messageToInt to its bits form and puts zero before to reach 8 characters
     messageToInt.forEach { messageToBits.add(Integer.toBinaryString(it.toInt()).padStart(8, '0')) }
     // It will join all the bits in one string and add the escape patron
-    val auxMessageToBits = messageToBits.joinToString("") + ESCAPE_PATRON
+    val auxMessageToBits = messageToBits.joinToString("")
 
     // returns an integers list with all the bits from the message and escapePatron
     return auxMessageToBits.map {it.toString().toInt()}.toList()
